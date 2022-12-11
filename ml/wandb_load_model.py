2131762931f97b1a550ad862eab4a139d1e47f0a
Shivalika Singh <73357305+shivalikasingh95@users.noreply.github.com>
2022-12-11 16:18:38 +0000

    added generate music inference logic, convert to mp3 and upload to s3

--- ml/wandb_load_model.py
+++ ml/wandb_load_model.py
@@ -2,13 +2,24 @@ import wandb
 from wandb.beta.workflows import use_model
 import torch
 
-# Startup a W&B Run
-wandb.init(project=project_name, 
-  job_type="model_inference",
-)
 
-model_art = wandb.use_artifact(f"{model_collection_name}:latest")
-model_path = model_art.get_path("checkpoint_last_linear_4096_chord_bpe_hardloss1_PI2.pt").download()
-model_obj = torch.load(model_path)
+def get_music_gen_model(project_name:str = "SymphonyNet"):
+  """
+  This function downloads the SymphonyNet pretrained checkpoint from Weights & Biases model registry
+  Args:
+    project_name (str): Name of project on Weights & Biases account under which SymphonyNet checkpoint was registered
+  Returns:
+    model_path (str): Path of the downloaded model file
+  """
+  model_collection_name = "Symphony"
+  # Startup a W&B Run
+  wandb.init(project=project_name, 
+    job_type="model_inference",
+  )
 
-wandb.finish()
+  model_art = wandb.use_artifact(f"{model_collection_name}:latest")
+  model_path = model_art.get_path("checkpoint_last_linear_4096_chord_bpe_hardloss1_PI2.pt").download()
+  # model_obj = torch.load(model_path)
+
+  wandb.finish()
+  return model_path
--- ml/wandb_model_log.py
+++ ml/wandb_model_log.py
@@ -1,21 +1,36 @@
+## This is a standalone script and you can use it to log the pretrained model checkpoint 
+## with Weights & Biases Model registry.
+## Edit the `model_ckpt_path` depending on the location where the ckpt 
+## is placed before running this script.
+
 import wandb
 from wandb.beta.workflows import log_model, link_model
 import torch
 
-
 project_name = "SymphonyNet"
 model_collection_name = "Symphony"
+model_ckpt_path = f"SymphonyNet/ckpt/checkpoint_last_linear_4096_chord_bpe_hardloss1_PI2.pt"
 
-# Startup a W&B Run
-wandb.init(project=project_name, 
-  job_type="log_trained_model",
-)
+def log_music_gen_model():
+  """
+  This function lets you log the pretrained SymphonyNet model checkpoint to Weights & Biases model registry
+  """
 
-is_best = True 
-art = wandb.Artifact(f"symphonynet-{wandb.run.id}", "model")
-art.add_file(f"SymphonyNet/ckpt/checkpoint_last_linear_4096_chord_bpe_hardloss1_PI2.pt")
-wandb.log_artifact(art, aliases=["best", "latest"] if is_best else None)
-wandb.run.link_artifact(art, model_collection_name, ["latest"])
+  # Startup a W&B Run
+  wandb.init(project=project_name, 
+    job_type="log_trained_model",
+  )
 
-# Finish the Run
-wandb.finish()
+  is_best = True 
+  art = wandb.Artifact(f"symphonynet-{wandb.run.id}", "model")
+  art.add_file(model_ckpt_path)
+  wandb.log_artifact(art, aliases=["best", "latest"] if is_best else None)
+  wandb.run.link_artifact(art, model_collection_name, ["latest"])
+
+  # Finish the Run
+  wandb.finish()
+
+if __name__ == '__main__':
+
+  log_music_gen_model()
+  
